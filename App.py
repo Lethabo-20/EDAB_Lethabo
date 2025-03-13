5ad89f9158fb045e77efcb82f35d5ba54b6330c6
Lethabo-20 <142802533+Lethabo-20@users.noreply.github.com>
2025-03-13 08:45:25 +0200

    Update App.py

--- App.py
+++ App.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
 st.title("This is my streamlit app for 13 March 2025")
-st.heading("The module code is EDAB6808")
-st.subheading("This is a fun and interactive module")
+st.header("The module code is EDAB6808")
+st.subheader("This is a fun and interactive module")
 
 # Generate random time series data
 time_series = np.random.randn(100)
