452a146e1b177d91a169fc2aa6b4fd0a2bf81a3a
Lethabo-20 <142802533+Lethabo-20@users.noreply.github.com>
2025-03-13 08:50:55 +0200

    Update App.py

--- App.py
+++ App.py
@@ -2,18 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
 
-st.title("This is my streamlit app for 13 March 2025")
-st.header("The module code is EDAB6808")
+st.title("This is my Streamlit app for 13 March")
+st.header("This module code is EDAB6808")
 st.subheader("This is a fun and interactive module")
-
 # Generate random time series data
 time_series = np.random.randn(100)
+
+
 variable = st.button("This is my button")
 if variable:
   time_series = np.random.randn(2000)
-  fig,ax = plt.subplot()
+  fig, ax = plt.subplots()
   ax.plot(time_series)
   ax.set_title("This is my graph title")
-  ax.ser_xlabel("Units")
+  ax.set_xlabel("Units")
   ax.set_ylabel("Value")
   st.pyplot(fig)
