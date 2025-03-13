15a05520aead29dbb306ba37fefe412a0e6a2e44
Lethabo-20 <142802533+Lethabo-20@users.noreply.github.com>
2025-03-13 08:38:14 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,12 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import streamlit as st
+st.title("This is my streamlit app for 13 March 2025")
+st.heading("The module code is EDAB6808")
+st.subheading("This is a fun and interactive module")
 
 # Generate random time series data
 time_series = np.random.randn(100)
-
-# Plot the time series
-plt.plot(time_series)
-plt.title("Random 100-Unit Time Series")
-plt.xlabel("Time")
-plt.ylabel("Value")
-plt.show()
+variable = st.button("This is my button")
+if variable:
+  time_series = np.random.randn(2000)
+  fig,ax = plt.subplot()
+  ax.plot(time_series)
+  ax.set_title("This is my graph title")
+  ax.ser_xlabel("Units")
+  ax.set_ylabel("Value")
+  st.pyplot(fig)
